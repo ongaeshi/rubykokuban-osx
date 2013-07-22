b02afcd9628372cee38d8757a5b5bd86e25806a2
ongaeshi <ongaeshi0621@gmail.com>
2013-07-23 01:08:36 +0900

    Move Console sample.rb to debuginfo.rb

--- apps/RubyBasicWorkspace/RubyBasic/src/sample.rb
+++ apps/RubyBasicWorkspace/RubyBasic/src/sample.rb
@@ -74,32 +74,4 @@ end
   
 # end
 
-class Console
-  def self.init(x, y, width = 600, height = 190, line_num = 13)
-    @x = x
-    @y = y
-    @width = width
-    @height = height
-    @line_num = line_num
-    @text = ""
-  end
-
-  def self.p(*args)
-    @text = args.map{|obj| obj.inspect }.join("\n") + "\n" + @text
-  end
-
-  def self.draw
-    @text = @text.split("\n")[0, @line_num].join("\n")
-    
-    set_fill
-    set_color(255, 255, 255)
-    rect(@x, @y, @width, @height)
-
-    set_no_fill
-    set_color(0, 0, 0)
-    rect(@x, @y, @width, @height)
-
-    text(@text, @x + 10, @y + 20)
-  end
-end
 
