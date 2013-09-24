eb451dea74743bd99fbc473628a9ad1be0f75aeb
ongaeshi <ongaeshi0621@gmail.com>
2013-09-25 02:26:12 +0900

    Add mosaic sample

--- apps/RubyKokubanWorkspace/RubyKokuban/src/sample.rb
+++ apps/RubyKokubanWorkspace/RubyKokuban/src/sample.rb
@@ -1,28 +1,24 @@
 def setup
   set_window_size(640, 480)
   set_background(255, 255, 255)
-
-  # Console.init(0, 0)
   set_fill
 
-  @image_org = Image.load("sample.png")
-  @image = @image_org.clone
+  @image = Image.load("sample.png")
 
-  # (0...@image.width).each do |x|
-  #   (0...@image.height).each do |y|
-  #     c = @image.color(x, y)
-  #     b = c.brightness
-  #     @image.set_color(x, y, Color.new(b, b, b, c.a))
-  #   end
-  # end
-
-  @image.each_pixels do |x, y|
-    c = self.color(x, y)        # Can't '@image.color(x, y)'. Why?
+  @image_gray = @image.clone
+  @image_gray.each_pixels do |x, y|
+    c = self.color(x, y)        # Can't '@image_gray.color(x, y)'. Why?
     b = c.brightness
     self.set_color(x, y, Color.new(b, b, b, c.a))
   end
+  @image_gray.update
 
-  @image.update
+  @mosaic_power = 1
+end
+
+def update
+  @mosaic_power -= 0.1
+  @mosaic_power = 16 if @mosaic_power < 1
 end
 
 def draw
@@ -30,28 +26,20 @@ def draw
   x = 10; y = 50
   description("original", x, y)
   set_color(Color::White)
-  @image_org.draw(x, y)
+  @image.draw(x, y)
 
   x = 110; y = 50
   description("gray", x, y)
   set_color(Color::White)
-  # (1..10).each do |x|
-  #   (0...@image.width).each do |x|
-  #     (0...@image.height).each do |y|
-  #       c = @image.color(x, y)
-  #       b = c.brightness
-  #       @image.set_color(x, y, Color.new(b, b, b, c.a))
-  #     end
-  #   end
-  # end
-  (1..10).each do |x|
-    @image.each_pixels do |x, y, c|
-      b = c.brightness
-      set_color(x, y, Color.new(b, b, b, c.a))
-    end
+  @image_gray.draw(x, y)
+
+  x = 210; y = 50
+  description("mosaic", x, y)
+  power = @mosaic_power
+  @image.each_pixels(power, power) do |i, j|
+    Kernel::set_color(self.color(i, j))
+    rect(x + i, y + j, power, power)
   end
-  @image.update
-  @image.draw(x, y)
 
   # debug info
   set_color(0, 0, 0)
